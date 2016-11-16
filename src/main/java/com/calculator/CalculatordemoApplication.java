59dd55f9a68795e011a614ba3f23fc5c700bbf19
bhiapp4 <bhiapp4@gmail.com>
2016-11-16 11:27:07 -0600

    Added a new calculator class and created a sysout in an existing class

--- src/main/java/com/calculator/CalculatordemoApplication.java
+++ src/main/java/com/calculator/CalculatordemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatordemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" Hello to spring boot");
 		SpringApplication.run(CalculatordemoApplication.class, args);
 	}
 }
