93bc9f70efd1ed08a2200ea2d6d90c76593e2428
bhiapp4 <bhiapp4@gmail.com>
2016-11-16 12:33:13 -0600

    Removed the sysout statement

--- src/main/java/com/calculator/CalculatordemoApplication.java
+++ src/main/java/com/calculator/CalculatordemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatordemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println(" Hello to spring boot");
 		SpringApplication.run(CalculatordemoApplication.class, args);
 	}
 }
